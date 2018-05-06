0d0248adafe7730c0acf9f8d7b4921a0e6f71fb2
Markus Petrykowski <markus.petrykowski@nexenio.com>
2018-05-07 00:08:43 +0200

    add some dummy elements

--- frontend/src/pages/Marketplace.jsx
+++ frontend/src/pages/Marketplace.jsx
@@ -4,20 +4,36 @@ import React, { Component } from 'react';
 import Container from '../components/Styled/Container';
 import TopBar from '../components/TopBar';
 import CarInformation from '../components/CarInformation';
+import CarCreation from '../components/CarCreationCard';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class Marketplace extends Component<{}> {
   render() {
     return (
       <MuiThemeProvider>
-        <Container fillColor={'#3f3f3f9c'} position={'absolute'} top={'0'}>
+        <Container position={'absolute'} top={'0'}>
           <TopBar />
-          <CarInformation
-            type={'Audi'}
-            description="Lorem Ipsum dolor sit amet, asdfkhasdjfh,d adfjh dhjsakh djasdjhf hdkjh djs dh hasjdoue dhjsk.!"
-            headline="Great Headline!"
-            price={200000}
-          />
+          <Container>
+            <CarCreation />
+            <CarInformation
+              type={'Audi'}
+              description="Lorem Ipsum dolor sit amet, asdfkhasdjfh,d adfjh dhjsakh djasdjhf hdkjh djs dh hasjdoue dhjsk.!"
+              headline="Great Headline!"
+              price={200000}
+            />
+            <CarInformation
+              type={'Audi'}
+              description="Lorem Ipsum dolor sit amet, asdfkhasdjfh,d adfjh dhjsakh djasdjhf hdkjh djs dh hasjdoue dhjsk.!"
+              headline="Great Headline!"
+              price={200000}
+            />
+            <CarInformation
+              type={'Audi'}
+              description="Lorem Ipsum dolor sit amet, asdfkhasdjfh,d adfjh dhjsakh djasdjhf hdkjh djs dh hasjdoue dhjsk.!"
+              headline="Great Headline!"
+              price={200000}
+            />
+          </Container>
         </Container>
       </MuiThemeProvider>
     );
