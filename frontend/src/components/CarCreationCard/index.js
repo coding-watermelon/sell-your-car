4d8f752978ac8629da4fc4228d5f7a47dcb65690
Markus Petrykowski <markus.petrykowski@nexenio.com>
2018-05-07 00:07:57 +0200

    Add component to add new car

---
+++ frontend/src/components/CarCreationCard/index.js
@@ -0,0 +1,53 @@
+// @flow
+import React from 'react';
+import Container from '../Styled/Container';
+import { media } from '../Styled/mediaStyles';
+import FlatButton from 'material-ui/FlatButton';
+import { Card, CardHeader, CardActions, CardText } from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+
+// Assets
+import addIcon from '../../assets/add.png';
+
+const CardContainer = Container.extend`
+  height: initial;
+  width: calc(100% - 2rem);
+  padding: 1rem;
+  display: inline-table;
+
+  ${media.desktop`
+    width: calc( 25% - 2rem );
+  `};
+`;
+
+type PropsType = {
+  add: () => void,
+};
+
+export default class CarInformation extends React.Component<PropsType> {
+  render() {
+    return (
+      <CardContainer>
+        <Card>
+          <CardHeader
+            title="Add new car"
+            subtitle="Enter information here"
+            avatar={addIcon}
+            actAsExpander={true}
+            showExpandableButton={true}
+            initiallyExpanded
+          />
+          <CardText expandable={true}>
+            <TextField hintText="Headline" fullWidth />
+            <TextField hintText="Type" fullWidth />
+            <TextField hintText="Price" type="number" fullWidth />
+            <TextField hintText="Description" fullWidth />
+          </CardText>
+          <CardActions expandable={true}>
+            <FlatButton label="Add" onClick={this.props.add} />
+          </CardActions>
+        </Card>
+      </CardContainer>
+    );
+  }
+}
