3b6578eb757d4a10b100fcaf80798c7e7c101160
Markus Petrykowski <markus.petrykowski@nexenio.com>
2018-05-07 18:31:24 +0200

    fetch initial data onLoad

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,6 +2,10 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
+// API Data
+import * as api from './api';
+import { addCar } from './reducers/cars';
+
 function makeStore(initialState, middlewares) {
   let enhancer = null;
   if (!(process.env.NODE_ENV === 'production')) {
@@ -15,5 +19,12 @@ function makeStore(initialState, middlewares) {
 
   return createStore(reducers, initialState, enhancer);
 }
+const store = makeStore({}, [thunk]);
 
-export default makeStore({}, [thunk]);
+api.fetchCars().then(cars => {
+  cars.forEach(car => {
+    store.dispatch(addCar(car));
+  });
+});
+
+export default store;
