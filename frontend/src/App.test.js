4c5c4c59e300aea3bbd18c3bd3ce843719bf980d
Markus Petrykowski <markus.petrykowski@nexenio.com>
2018-05-07 21:58:00 +0200

    Add Provider to App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
