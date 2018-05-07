445a8a115ba84bef5434573a7a1ae92d630fd258
Markus Petrykowski <markus.petrykowski@nexenio.com>
2018-05-07 09:04:20 +0200

    add redux store to application

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Redux
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
+
+// Application
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
