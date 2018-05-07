91e4b87db126db0ecb47da81812d41057d768c9f
Markus Petrykowski <markus.petrykowski@nexenio.com>
2018-05-07 21:51:22 +0200

    add filter to global state and export ReduxActionType

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,4 +1,7 @@
 import { combineReducers } from 'redux';
 import cars from './cars';
+import filter from './filter';
 
-export default combineReducers({ cars });
+export type ReduxActionType = { type: string, payload?: {} };
+
+export default combineReducers({ cars, filter });
