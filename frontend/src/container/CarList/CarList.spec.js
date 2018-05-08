7b74919174257e561610b3a6f9e6e047f46fe364
Markus Petrykowski <markus.petrykowski@nexenio.com>
2018-05-08 10:11:45 +0200

    Test CarList Component

---
+++ frontend/src/container/CarList/CarList.spec.js
@@ -0,0 +1,30 @@
+// @flow
+import React from 'react';
+import { cars } from '../../test/carFactory';
+import CarList from './CarList';
+import CarCreation from '../../components/CarCreationCard';
+import CarInformation from '../../components/CarInformation';
+import { shallow } from 'enzyme';
+
+describe('CarList', () => {
+  it('should render correctly', () => {
+    const component = shallow(<CarList cars={[]} />);
+
+    expect(component.find(CarCreation)).toHaveLength(1);
+    expect(component.find(CarInformation)).toHaveLength(0);
+  });
+
+  it('should render a list of cars', () => {
+    const carItems = cars(3);
+    const component = shallow(<CarList cars={carItems} />);
+
+    expect(component.find(CarInformation)).toHaveLength(3);
+
+    // Check whether props are correctly passed through
+    component
+      .find(CarInformation)
+      .forEach((CarListItem: CarInformation, index: number) => {
+        expect(CarListItem.props()).toEqual(carItems[index]);
+      });
+  });
+});
